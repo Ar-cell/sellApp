38c540e523e6d083a8a326255a8c4adfdb7b1b21
Arun Mudgal <58321357+Ar-cell@users.noreply.github.com>
2022-12-16 16:31:27 +0530

    Initial commit

---
+++ src/main/java/com/sellAppDemo/entity/Admin.java
@@ -0,0 +1,30 @@
+package com.sellAppDemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String  email;
+
+    private String phoneno;
+
+    private String address;
+}
